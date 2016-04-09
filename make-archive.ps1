b15dd7a188e295583838e0996b307acbc32dad7b
strawgate <williamseaston@gmail.com>
2016-04-09 12:14:31 -0500

    Fixing output path for make-archive

--- make-archive.ps1
+++ make-archive.ps1
@@ -6,7 +6,7 @@ function Invoke-Archive {
     param (
         [IO.DirectoryInfo] $Folder
     )
-    .\BFArchive.exe -a "$($Folder.FullName)" "$($Folder.name).bftemp"
+    .\BFArchive.exe -a "$($Folder.FullName)" "$($Folder.FullName).bftemp"
 }
 
 $Folder = get-item $InFolder
